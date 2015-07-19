d32d20e6da3be04b47c675c6e711e2949ce159cd
Rufer Marc <rufermarc@gmail.com>
2015-07-19 21:49:41 +0200

    initial javascript commit

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,17 +1,13 @@
-var xAuthTokenHeaderName = 'x-auth-token';
-
-angular.module('gameSchedulePdfCreatorApp', ['ionic', 'ngCookies', 'exampleApp.services'])
+angular.module('gameSchedulePDFCreatorApp', ['ionic', 'gameSchedulePDFCreatorApp.services'])
     .config(
     ['$stateProvider', '$urlRouterProvider', '$httpProvider', function ($stateProvider, $urlRouterProvider, $httpProvider) {
 
-        $stateProvider.state('create', {
-            url: '/create',
-            templateUrl: 'partials/create.html',
-            controller: CreateController
-        })
-            .state('edit', {url: '/edit/:id', templateUrl: 'partials/edit.html', controller: EditController})
-            .state('login', {url: '/login', templateUrl: 'partials/login.html', controller: LoginController})
-            .state('index', {url: '/index', templateUrl: 'partials/index.html', controller: IndexController});
+        $stateProvider.state('index', {
+                url: '/index',
+                templateUrl: 'partials/index.html',
+                controller: IndexController
+            }
+        );
 
         $urlRouterProvider.otherwise('/index');
 
@@ -42,128 +38,32 @@ angular.module('gameSchedulePdfCreatorApp', ['ionic', 'ngCookies', 'exampleApp.s
                 return promise.then(success, error);
             };
         };
-        $httpProvider.responseInterceptors.push(interceptor);
+        $httpProvider.interceptors.push(interceptor);
 
         $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
     }]
-).run(function ($rootScope, $http, $location, $cookieStore, LoginService) {
+).run(function ($rootScope, $http, PDFCreatorService) {
 
         /* Reset error when a new view is loaded */
         $rootScope.$on('$viewContentLoaded', function () {
             delete $rootScope.error;
         });
-
-        $rootScope.hasRole = function (role) {
-
-            if ($rootScope.user === undefined) {
-                return false;
-            }
-
-            if ($rootScope.user.roles[role] === undefined) {
-                return false;
-            }
-
-            return $rootScope.user.roles[role];
-        };
-
-        $rootScope.logout = function () {
-            delete $rootScope.user;
-            delete $http.defaults.headers.common[xAuthTokenHeaderName];
-            $cookieStore.remove('user');
-            $location.path("/login");
-        };
-
-        /* Try getting valid user from cookie or go to login page */
-        var originalPath = $location.path();
-        $location.path("/login");
-        var user = $cookieStore.get('user');
-        if (user !== undefined) {
-            $rootScope.user = user;
-            $http.defaults.headers.common[xAuthTokenHeaderName] = user.token;
-            $location.path(originalPath);
-        }
-
     });
 
 
-function IndexController($scope, $state, NewsService) {
+function IndexController($scope, $state, PDFCreatorService, SwissunihockeyAPIService) {
 
-    $scope.newsEntries = NewsService.query();
-
-    $scope.data = {
-        showDelete: false
-    };
-
-    $scope.deleteEntry = function (newsEntry) {
-        newsEntry.$remove(function () {
-            $scope.newsEntries = NewsService.query();
-        });
-    };
-
-    $scope.edit = function (newsEntry) {
-        $state.go('edit', {id: newsEntry.id});
-    };
+    $scope.clubEntries = SwissunihockeyAPIService.query();
 }
 
+var services = angular.module('gameSchedulePDFCreatorApp.services', ['ngResource']);
 
-function EditController($scope, $stateParams, $location, NewsService) {
+services.factory('PDFCreatorService', function ($resource) {
 
-    $scope.newsEntry = NewsService.get({id: $stateParams.id});
-
-    $scope.save = function () {
-        $scope.newsEntry.$save(function () {
-            $location.path('/');
-        });
-    };
-}
-
-
-function CreateController($scope, $location, NewsService) {
-
-    $scope.newsEntry = new NewsService();
-
-    $scope.save = function () {
-        $scope.newsEntry.$save(function () {
-            $location.path('/');
-        });
-    };
-}
-
-
-function LoginController($scope, $rootScope, $location, $http, $cookieStore, LoginService) {
-    $scope.credentials = {
-        username: '', password: ''
-    };
-
-    $scope.login = function () {
-        LoginService.authenticate({
-            username: $scope.credentials.username,
-            password: $scope.credentials.password
-        }, function (user) {
-            $rootScope.user = user;
-            $http.defaults.headers.common[xAuthTokenHeaderName] = user.token;
-            $cookieStore.put('user', user);
-            $location.path("/");
-        });
-    };
-}
-
-
-var services = angular.module('exampleApp.services', ['ngResource']);
-
-services.factory('LoginService', function ($resource) {
-
-    return $resource(':action', {},
-        {
-            authenticate: {
-                method: 'POST',
-                params: {'action': 'authenticate'}
-            }
-        }
-    );
+    return $resource('/api/clubs/:clubId/teams/:teamId/game-schedule', {clubId: '@clubId', teamId: '@teamId'});
 });
 
-services.factory('NewsService', function ($resource) {
+services.factory('SwissunihockeyAPIService', function ($resource) {
 
-    return $resource('news/:id', {id: '@id'});
+    return $resource('https://api-v2.swissunihockey.ch/api/clubs');
 });
